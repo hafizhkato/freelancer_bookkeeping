54b6d611527abff2a1925d0bfdc6214ea2e197d4
Hafizh Zulkifli <hafizhzulkifli@gmail.com>
2025-03-15 13:44:54 +0800

    add invoice table

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -9,20 +9,18 @@ const schema = a.schema({
       clientPhoneNumber: a.phone(),
       clientAddress: a.string(),
     })
-
-  // InvoiceTable: a
-  //   .model({
-  //     invoiceId: a.id().required(),
-  //     clientId: a.id(),
-  //     invoiceDate: a.date(),
-  //     dueDate: a.date(),
-  //     amount: a.float(),
-  //     status: a.enum(['Pending', 'Paid', 'Cancel', 'Overdue']),
-
-  //   })
-    
-
     .identifier(['clientId', 'clientName'])
+    .authorization((allow) => [allow.owner()]),
+
+    InvoiceTable: a
+    .model({
+      invoiceId: a.id().required(),
+      clientId: a.id().required(),
+      invoiceDate: a.date(),
+      amount: a.float(),
+      status: a.enum(['Pending', 'Paid', 'Overdue', 'Cancel']),
+    })
+    .identifier(['invoiceId', 'clientId'])
     .authorization((allow) => [allow.owner()]),
 });
 
@@ -35,31 +33,4 @@ export const data = defineData({
   },
 });
 
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
