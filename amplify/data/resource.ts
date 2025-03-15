a5ac1d77a6aafd4edd24b51556f4f1a312fe297a
Hafizh Zulkifli <hafizhzulkifli@gmail.com>
2025-03-15 16:46:59 +0800

    add expense table, and client document

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -8,8 +8,21 @@ const schema = a.schema({
       clientEmail: a.email(),
       clientPhoneNumber: a.phone(),
       clientAddress: a.string(),
+      description: a.string(),
+      members: a.hasMany('ClientDocuments', ['clientId','clientName'])
     })
     .identifier(['clientId', 'clientName'])
+    .authorization((allow) => [allow.owner()]),
+
+    ClientDocuments: a
+    .model({
+      title: a.string().required(),
+      date: a.date(),
+      documentType: a.string(),
+      documentURL: a.url().required(),
+      description: a.string(),
+      document: a.belongsTo('ClientTable',['clientId','clientName'])
+    })
     .authorization((allow) => [allow.owner()]),
 
     InvoiceTable: a
@@ -19,12 +32,13 @@ const schema = a.schema({
       invoiceDate: a.date(),
       amount: a.float(),
       status: a.enum(['Pending', 'Paid', 'Overdue', 'Cancel']),
+      description: a.string(),
+      invoiceURL: a.url(),
       members: a.hasMany('InvoiceItem', ['invoiceId','clientId'])
     })
     .identifier(['invoiceId', 'clientId'])
     .authorization((allow) => [allow.owner()]),
    
-
     InvoiceItem: a
     .model({
       itemName: a.string().required(),
@@ -33,8 +47,33 @@ const schema = a.schema({
       clientId: a.id().required(),
       itemPrice: a.float().required(),
       totalAmount: a.float().required(),
-      Description: a.string(),
+      description: a.string(),
       invoice: a.belongsTo('InvoiceTable', ['invoiceId', 'clientId'])
+    })
+    .authorization((allow) => [allow.owner()]),
+
+    ExpenseTable: a
+    .model({
+      expenseId: a.id().required(),
+      expenseDate: a.date(),
+      category: a.string(),
+      amount: a.float().required(),
+      description: a.string(),
+      receiptURL: a.url(),
+      members: a.hasMany('ExpenseItem', 'expenseId')
+    })
+    .identifier(['expenseId'])
+    .authorization((allow) => [allow.owner()]),
+
+    ExpenseItem: a
+    .model({
+      itemName: a.string().required(),
+      itemQuantity: a.integer().required(),
+      expenseId: a.id().required(),
+      itemPrice: a.float().required(),
+      totalAmount: a.float().required(),
+      description: a.string(),
+      expense: a.belongsTo('ExpenseTable','expenseId')
     })
     .authorization((allow) => [allow.owner()]),
 
